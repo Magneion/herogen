7957f105f6db74d0a9f8e209fc7d5accdbd027b7
popschool <popschool@debian>
2017-07-10 16:04:57 +0200

    end work 10 07

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var creation = function () {
+  heroes.innerHTML = "document.getElementsById('name').value" + " "+
+  "document.getElementsById('realid').value" +
+  "document.getElementById('dex').value" +
+  "document.getElementById('str').value" +
+  "document.getElementById('con').value";
+}
